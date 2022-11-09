ba2fbb0dc1b4659549306f67149c5a1b4af92b6f
Alex Yevtushenko <a@qx.zone>
2022-11-09 23:38:25 +0200

    Add FastLED Add base config

---
+++ src/config.h
@@ -0,0 +1,14 @@
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define PIN_DOOR_BELL             2
+#define PIN_DOOR_LOCK             17
+#define PIN_DOOR_SENSOR           27
+#define PIN_BATTERY_LEVEL         4
+#define PIN_EXTRA_1               25
+#define PIN_EXTRA_2               26
+
+#define PIN_LED                   PIN_EXTRA_1
+#define LED_COUNT                 9
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include "version.h"
+#include "config.h"
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoOTA.h>
+#include <FastLED.h>
+
+CRGB leds[LED_COUNT];
 
 void setup() {
+  pinMode(PIN_DOOR_LOCK, OUTPUT);
 
+  FastLED.addLeds<WS2813, PIN_LED, RGB>(leds, LED_COUNT); 
+  FastLED.setBrightness(255);
 }
 
 void loop() {
-  
+	leds[0] = CRGB::Cyan; 
+  FastLED.show(); 
+  delay(30);
+	
+  // leds[0] = CRGB::Black; 
+  // FastLED.show(); 
+  // delay(30);
 }
