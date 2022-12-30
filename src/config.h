b9f475f69abf2202ef46a7e52374709d719222a7
AY <a@qx.zone>
2022-12-30 15:17:18 +0200

    Basic deep sleep implementation (#2)
    * Basic deep sleep implementation
    * Stable implementation
    * Publish LED state periodically
    * Improve OTA
    * Improve MQTT

--- src/config.h
+++ src/config.h
@@ -1,14 +1,21 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#define PIN_DOOR_BELL             2
-#define PIN_DOOR_LOCK             17
-#define PIN_DOOR_SENSOR           27
-#define PIN_BATTERY_LEVEL         32
-#define PIN_EXTRA_1               25
-#define PIN_EXTRA_2               26
+#define PIN_DOOR_BELL             GPIO_NUM_2
+#define PIN_DOOR_LOCK             GPIO_NUM_17
+#define PIN_DOOR_SENSOR           GPIO_NUM_27
+#define PIN_BATTERY_LEVEL         GPIO_NUM_32
+#define PIN_EXTRA_1               GPIO_NUM_25
+#define PIN_EXTRA_2               GPIO_NUM_26
+
+#ifdef DEBUG
+#define LED                       2
+#endif
 
 #define PIN_LED                   PIN_EXTRA_1
+#define PIN_LED_POWER             PIN_DOOR_SENSOR
+#define LED_POWER_ON              LOW
+#define LED_POWER_OFF             HIGH
 #define LED_COUNT                 9
 
 #define LED_TIMEOUT_MS            15000
@@ -17,5 +24,12 @@
 
 #define BATTERY_VOLTAGE_UPDATE_MS 30000
 #define BATTERY_VOLTAGE_READ_MS   5000
+
+#define DEEP_SLEEP_TIME_US        3 * 1000000
+#define MIN_RUN_TIME_MS           200
+#define MAX_RUN_TIME_WAIT_WIFI_MS 10 * MIN_RUN_TIME_MS
+
+#define DOOR_LOCK_IMPULSE_MS      500
+#define DOOR_LOCK_MIN_DELAY_MS    30000
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,6 @@
 #include <SwitchRelay.h>
 #include <ble-lock.h>
 
-RTC_DATA_ATTR static time_t boot_last_time;        // remember last boot in RTC Memory
-RTC_DATA_ATTR static uint32_t boot_count; // remember number of boots in RTC Memory
-
 const String pubsub_topic_lock = String(MQTT_TOPIC_PREFIX "/lock/set");
 const String pubsub_topic_light = String(MQTT_TOPIC_PREFIX "/light/set");
 const String pubsub_topic_restart = String(MQTT_TOPIC_PREFIX "/restart");
@@ -23,32 +20,53 @@ CRGB
   current_color = CRGB::Black;
 
 bool
-  ledOn = false;
+  publishDoorLock0 = false,
+  publishMotion1 = false,
+  publishBatteryLevel = false,
+  canSleep = true,
+  otaStarted = false,
+  wifiWasConnected = false;
 
-uint16_t
-  batteryLevel = 0;
+unsigned int
+  messagesReceived = 0;
 
 unsigned long
+  minRunTimeMs = MIN_RUN_TIME_MS,
+  now_global_start = 0,
+  now = 0;
+
+RTC_DATA_ATTR unsigned long 
   lastLedOn = 0,
   lastBatteryVoltageReadMs = 0,
   lastBatteryVoltageUpdateMs = 0,
-  lastDoorLockActivatedMs = 0,
-  now = 0;
+  lastDoorLockOnMs = 0,
+  now_global = 0;
 
-struct timeval now_time;
+RTC_DATA_ATTR uint16_t
+  boot_count = 0,
+  batteryLevel = 0;
+
+RTC_DATA_ATTR bool
+  ledOn = false;
 
 void light_on(CRGB color) {
   if (ledOn && color == current_color) {
-    lastLedOn = now;
+    lastLedOn = now_global;
     return;
   }
 
   log_i("light ON");
-
   ledOn = true;
-  lastLedOn = now;
+  lastLedOn = now_global;
+
+  digitalWrite(PIN_LED_POWER, LED_POWER_ON);
+
   current_color = color;
   FastLED.showColor(CRGB::Yellow);
+
+  #ifdef DEBUG
+  digitalWrite(LED, HIGH);
+  #endif
 
   pubSubClient.publish(MQTT_TOPIC_PREFIX "/light", "1");
 }
@@ -63,11 +81,17 @@ void light_off() {
   ledOn = false;
   FastLED.clear(true);
 
+  digitalWrite(PIN_LED_POWER, LED_POWER_OFF);
+
+  #ifdef DEBUG
+  digitalWrite(LED, LOW);
+  #endif
+
   pubSubClient.publish(MQTT_TOPIC_PREFIX "/light", "0");
 }
 
 void light_loop() {
-  if (ledOn && now - lastLedOn > LED_TIMEOUT_MS) {
+  if (ledOn && now_global - lastLedOn > LED_TIMEOUT_MS) {
     light_off();
   }
 }
@@ -77,72 +101,107 @@ void door_lock_open() {
   light_on();
 
   pubSubClient.publish(MQTT_TOPIC_PREFIX "/lock", "U");
-  digitalWrite(PIN_DOOR_LOCK, HIGH);
-  lastDoorLockActivatedMs = now;
 
-  // // door_lock.setOn();
-  // delay(500);
-  // digitalWrite(PIN_DOOR_LOCK, LOW);
-  // // door_lock.setOff();
+  digitalWrite(PIN_DOOR_LOCK, HIGH);
+  delay(500);
+  digitalWrite(PIN_DOOR_LOCK, LOW);
+
+  pubSubClient.publish(MQTT_TOPIC_PREFIX "/lock", "S");
+  // publishDoorLock0 = true;
+  canSleep = false;
 }
 
 void on_motion_state(MotionState_t state) {
   if (state == MotionState_t::Detected) {
     log_i("Motion detected!");
     light_on();
-  }
 
-  pubSubClient.publish(MQTT_TOPIC_PREFIX "/motion", state == Detected ? "1" : "0");
+    publishMotion1 =  true;
+    canSleep = false;
+  }
 }
 
 void battery_voltage_loop() {
-  if (now - lastBatteryVoltageReadMs > BATTERY_VOLTAGE_READ_MS) {
-    lastBatteryVoltageReadMs = now;
-    batteryLevel = (batteryLevel + analogRead(PIN_BATTERY_LEVEL)) / 2;
+  if (now_global - lastBatteryVoltageReadMs > BATTERY_VOLTAGE_READ_MS) {
+    if (lastBatteryVoltageReadMs > 0) batteryLevel = (batteryLevel + analogRead(PIN_BATTERY_LEVEL)) / 2;
+    else batteryLevel = analogRead(PIN_BATTERY_LEVEL);
+    lastBatteryVoltageReadMs = now_global;
   }
 
-  if (now - lastBatteryVoltageUpdateMs > BATTERY_VOLTAGE_UPDATE_MS) {
-    lastBatteryVoltageUpdateMs = now;
+  if (now_global - lastBatteryVoltageUpdateMs > BATTERY_VOLTAGE_UPDATE_MS) {
+    lastBatteryVoltageUpdateMs = now_global;
 
-    pubSubClient.publish(MQTT_TOPIC_PREFIX "/battery/raw", String(batteryLevel).c_str());
-  }
-}
-
-void door_lock_loop() {
-  if (lastDoorLockActivatedMs > 0 && now - lastDoorLockActivatedMs > 500) {
-    lastDoorLockActivatedMs = 0;
-
-    digitalWrite(PIN_DOOR_LOCK, LOW);
-    // door_lock.setOff();
-
-    pubSubClient.publish(MQTT_TOPIC_PREFIX "/lock", "S");
+    publishBatteryLevel = true;
   }
 }
 
 void on_pubsub_message(char* topic, uint8_t* data, unsigned int length) {
   log_d("MQTT message: topic=%s; length=%u; data=%s", topic, length, data);
+  messagesReceived++;
+
   if (pubsub_topic_lock.equals(topic) && parse_bool_meesage(data, length)) {
-    door_lock_open();
+    if (lastDoorLockOnMs == 0 || now_global - lastDoorLockOnMs > DOOR_LOCK_MIN_DELAY_MS) {
+      lastDoorLockOnMs = now_global;
+
+      // pubSubClient.publish(pubsub_topic_lock.c_str(), "0");
+      door_lock_open();
+    }
   }
   else if (pubsub_topic_light.equals(topic) && parse_bool_meesage(data, length)) {
+    // pubSubClient.publish(pubsub_topic_light.c_str(), "0");
     light_on();
   }
-  else if (pubsub_topic_restart.equals(topic) && parse_bool_meesage(data, length)) {
-    ESP.restart();
+  // else if (pubsub_topic_restart.equals(topic) && parse_bool_meesage(data, length)) {
+  //   ESP.restart();
+  // }
+}
+
+void on_ota_start() {
+  otaStarted = true;
+  canSleep = false;
+}
+
+void on_ota_error(ota_error_t err) {
+  log_w("OTA ERROR: code=%u", err);
+  esp_restart();
+}
+
+void print_wakeup_reason() {
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : log_i("- wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : log_i("- wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : log_i("- wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : log_i("- wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : log_i("- wakeup caused by ULP program"); break;
+    default : log_i("- wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
   }
 }
 
 void setup() {
   log_i("BOOT #%u", ++boot_count);
+  print_wakeup_reason();
 
-  gettimeofday(&now_time, NULL);
-  boot_last_time = now_time.tv_sec;
-  log_i("  %lds since last reset, %lds since last boot", now_time.tv_sec, now_time.tv_sec - boot_last_time);
+  if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER) {
+    now_global += DEEP_SLEEP_TIME_US / 1000;
+  }
 
-  log_i("SETUP start");
+  now_global_start = now_global;
+
+  log_i("SETUP START");
   pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_DOOR_LOCK, OUTPUT);
   pinMode(PIN_BATTERY_LEVEL, INPUT);
+  pinMode(PIN_LED_POWER, OUTPUT);
+
+  #ifdef DEBUG
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, ledOn);
+  #endif
 
   // ble_lock_setup();
 
@@ -151,25 +210,82 @@ void setup() {
   FastLED.addLeds<WS2812B, PIN_LED, RGB>(leds, LED_COUNT);
   FastLED.setBrightness(255);
 
-  wifi_setup();
+  wifi_setup(boot_count == 1);
   pubSubClient.setCallback(on_pubsub_message);
 
+  ArduinoOTA.onStart(on_ota_start);
+  ArduinoOTA.onError(on_ota_error);
+  ArduinoOTA.setRebootOnSuccess(true);
+  ArduinoOTA.setMdnsEnabled(false);
   ArduinoOTA.begin();
+
   log_i("SETUP done");
 }
 
 void loop() {
   now = millis();
+  now_global = now_global_start + now;
+  canSleep = true;
 
   mot.loop();
   light_loop();
   battery_voltage_loop();
-  door_lock_loop();
   // ble_lock_loop();
 
   if (wifi_loop(now)) {
+    if (!wifiWasConnected) canSleep = false;
+
+    wifiWasConnected = true;
+
     ArduinoOTA.handle();
+    if (otaStarted) return;
+
+    if (mqtt_loop(now)) {
+      if (publishDoorLock0) publishDoorLock0 = !pubSubClient.publish(MQTT_TOPIC_PREFIX "/lock", "S");
+      if (publishMotion1) publishMotion1 = !pubSubClient.publish(MQTT_TOPIC_PREFIX "/motion", "1");
+      if (publishBatteryLevel) publishBatteryLevel = 
+        !pubSubClient.publish(MQTT_TOPIC_PREFIX "/battery/raw", String(batteryLevel).c_str()) ||
+        !pubSubClient.publish(MQTT_TOPIC_PREFIX "/light", ledOn ? "1" : "0") ||
+        !pubSubClient.publish(MQTT_TOPIC_PREFIX "/motion", mot.getState() == Detected ? "1" : "0");
+    }
   }
 
-  delay(200);
+  if (canSleep && now > minRunTimeMs) {
+    if ((!wifiWasConnected || publishDoorLock0 || publishMotion1 || publishBatteryLevel) && minRunTimeMs < MAX_RUN_TIME_WAIT_WIFI_MS) {
+      minRunTimeMs += 100;
+    }
+    else {
+      if (wifiWasConnected) {
+        if (pubSubClient.connected()) {
+          log_d("MQTT flush");
+          for (uint8_t i=0; i<20 && !pubSubClient.loop(); i++) { delay(5); }
+          pubSubClient.flush();
+        }
+
+        // log_d("WIFI disconnect");
+        // for (uint8_t i=0; i<20 && !WiFi.disconnect(false); i++) { delay(5); }
+      }
+
+      auto m = millis();
+      now_global = now_global_start + m;
+
+#ifdef DEBUG
+      log_d("ENTER DEEP SLEEP: wifi=%s; battery=%u; messages=%u; led=%s; global_runtime=%u; runtime=%u", 
+        wifiWasConnected ? "true" : "false", 
+        batteryLevel, 
+        messagesReceived, 
+        ledOn ? "on" : "off", 
+        now_global, 
+        m);
+
+      Serial.flush(true);
+      delay(10);
+#endif
+
+      esp_sleep_enable_ext0_wakeup(PIN_MOTION_SENSOR, HIGH);
+      esp_deep_sleep(DEEP_SLEEP_TIME_US);
+    }
+  }
+
+  delay(50);
 }
